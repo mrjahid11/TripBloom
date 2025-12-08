01e7ee20da01d06dab01a0797c04369ccc99b655
mrjahid11 <captainjahid45@gmail.com>
2025-12-09 02:38:20 +0600

    App component and main entry point with routing and styles

---
+++ frontend/src/App.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import TourPackagesManager from './components/TourPackagesManager';
+import UsersManager from './components/UsersManager';
+import Hero from './components/Hero';
+import HowItWorks from './components/HowItWorks';
+import PopularDestinations from './components/PopularDestinations';
+import TourCategories from './components/TourCategories';
+import WhyChooseUs from './components/WhyChooseUs';
+import RefundsManager from './components/RefundsManager';
+import ReviewsManager from './components/ReviewsManager';
+import SettingsManager from './components/SettingsManager';
+import Reviews from './components/Reviews';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import AdminDashboard from './components/AdminDashboard';
+import AdminLayout from './components/AdminLayout';
+import CustomerDashboard from './components/CustomerDashboard';
+import OperatorDashboard from './components/OperatorDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
+import GroupDeparturesManager from './components/GroupDeparturesManager';
+import OperatorsManager from './components/OperatorsManager';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <div className="App bg-white dark:bg-gray-900 transition-colors duration-300">
+            <Navbar />
+            <Hero />
+            <HowItWorks />
+            <PopularDestinations />
+            <TourCategories />
+            <WhyChooseUs />
+            <Reviews />
+            <Contact />
+            <Footer />
+          </div>
+        } />
+        <Route path="/admin" element={
+          <ProtectedRoute role="admin">
+            <AdminLayout />
+          </ProtectedRoute>
+        }>
+          <Route index element={<AdminDashboard />} />
+          <Route path="departures" element={<GroupDeparturesManager />} />
+          <Route path="operators" element={<OperatorsManager />} />
+          <Route path="packages" element={<TourPackagesManager />} />
+          <Route path="users" element={<UsersManager />} />
+          <Route path="refunds" element={<RefundsManager />} />
+          <Route path="reviews" element={<ReviewsManager />} />
+          <Route path="settings" element={<SettingsManager />} />
+        </Route>
+        <Route path="/customer" element={
+          <ProtectedRoute role="customer">
+            <CustomerDashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/operator" element={
+          <ProtectedRoute role="operator">
+            <OperatorDashboard />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
